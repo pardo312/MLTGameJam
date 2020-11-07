23760070846345075735200acdb5bf7ea381a636
Jiufen <da.pardo@uniandes.edu.co>
2020-11-07 10:28:57 -0500

    Transicion entre cuerpos y arboles

---
+++ Assets/Scripts/Game/ClickOnBody.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOnBody : MonoBehaviour
+{
+    [SerializeField, Range(1,4)]private int bodyNumber;
+    private int transitionSpeed = 1;
+    private GameObject associatedTree;
+    private GameObject puzzleUI;
+    private bool transitionOn;
+    private void Start() {
+        associatedTree = GameObject.Find("Tree"+bodyNumber);
+        puzzleUI = GameObject.Find("PuzzleUI");
+        transitionOn=false;
+    }
+    void OnMouseUp() 
+    {
+        transitionOn=true;
+    } 
+    void LateUpdate () {
+        if(transitionOn)
+        {
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position ,new Vector3(associatedTree.transform.position.x,associatedTree.transform.position.y,-10),Time.deltaTime*transitionSpeed);
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize,3,Time.deltaTime*transitionSpeed);
+            if(Camera.main.orthographicSize==3)
+            {
+                transitionOn=false;
+                puzzleUI.SetActive(true);
+            }
+        }
+
+    }
+}
