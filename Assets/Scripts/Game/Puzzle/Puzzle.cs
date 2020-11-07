76d0d6e709ea6f31c8024e4225e9538049f229cc
Jiufen <da.pardo@uniandes.edu.co>
2020-11-07 17:49:02 -0500

    Solucion no sirve, muy complicada. Emepezando projecto de 0

--- Assets/Scripts/Game/Puzzle/Puzzle.cs
+++ Assets/Scripts/Game/Puzzle/Puzzle.cs
@@ -4,29 +4,33 @@ using UnityEngine.UI;
 public class Puzzle : MonoBehaviour
 {
     private int tilesPerLine = 3;
-    [SerializeField]private GameObject tileGameObject;
+    [SerializeField] private GameObject tileGameObject;
     private Tile emptyTile;
     private Tile selectedTile;
-    private Vector2 mouseTileRatio =Vector2.zero;
+    private Vector2 mouseTileDistance = Vector2.zero;
+    private Vector3 initMousePosition = Vector2.zero;
 
     private float offset = 110f;
-    private void Start() {
+    private void Start()
+    {
         createPuzzleTiles();
     }
     void createPuzzleTiles()
     {
-        for(int col = 0; col<tilesPerLine;col++){
-            for(int row = 0; row<tilesPerLine;row++){
+        for (int col = 0; col < tilesPerLine; col++)
+        {
+            for (int row = 0; row < tilesPerLine; row++)
+            {
                 GameObject tileObject = GameObject.Instantiate(tileGameObject);
                 tileObject.transform.parent = this.transform;
-                tileObject.transform.localPosition = new Vector3(row*(100 + offset)-(this.GetComponent<Image>().rectTransform.rect.width/2),col*(100 + offset)-320+ offset,0);
+                tileObject.transform.localPosition = new Vector3(row * (100 + offset) - (this.GetComponent<Image>().rectTransform.rect.width / 2), col * (100 + offset) - 320 + offset, 0);
 
 
                 Tile tile = tileObject.GetComponent<Tile>();
                 tile.OnTilePressed += moveTileInput;
                 tile.OnTileReleased += releaseTileInput;
 
-                if (col == 0 && row == tilesPerLine -1)
+                if (col == 0 && row == tilesPerLine - 1)
                 {
                     tileObject.SetActive(false);
                     emptyTile = tile;
@@ -34,50 +38,53 @@ public class Puzzle : MonoBehaviour
             }
         }
     }
-    void moveTileInput(Tile tileToMove){
+    void moveTileInput(Tile tileToMove)
+    {
         selectedTile = tileToMove;
-        
-    } 
-    void releaseTileInput(Tile tileToMove){
+        initMousePosition = Input.mousePosition;
+    }
+    void releaseTileInput(Tile tileToMove)
+    {
         selectedTile = null;
     }
-    
-    private void Update() {
-        if(selectedTile!=null)
+
+    private void Update()
+    {
+        if (selectedTile != null)
         {
-            Canvas myCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
-            Vector2 Mousepos;
-            Vector2 Tilepos;
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, myCanvas.worldCamera, out Mousepos);
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, selectedTile.initPosition, myCanvas.worldCamera, out Tilepos);
-            if(mouseTileRatio==Vector2.zero){
-                mouseTileRatio = Mousepos-Tilepos;
-            }
-            Vector2 currentMouse = myCanvas.transform.TransformPoint(Mousepos-mouseTileRatio);
-            
-            Vector2 emptyTilePos = emptyTile.transform.position;
-            Vector2 CurrentPosition = (Vector2)selectedTile.transform.position;
+           
+                Canvas myCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+                Vector2 Mousepos;
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, myCanvas.worldCamera, out Mousepos);
 
-            Debug.Log("CurrentPosition: " + (int)currentMouse.x);
-            // Debug.Log("selectedTile: " + selectedTile.transform.localPosition);
-            // Debug.Log("MousePosX: "+MousePosition.x);
+                mouseTileDistance = (Vector2)initMousePosition - selectedTile.initPosition;
 
-            Debug.Log("InitLimitX: "+(int)selectedTile.initPosition.x);
-            Debug.Log("EmptyLimitX: "+(int)(emptyTilePos.x));
-            // Debug.Log("InitLimitY: "+(int)selectedTile.initPosition.y);
-            // Debug.Log("EmptyLimitY: "+(int)(emptyTilePos.y));
+                Vector2 currentTilePosition = myCanvas.transform.TransformPoint(Mousepos - mouseTileDistance);
 
-            if((currentMouse.x <= Mathf.Max(emptyTilePos.x,selectedTile.initPosition.x)
-            && currentMouse.x >= Mathf.Min(emptyTilePos.x,selectedTile.initPosition.x))&&
-            (currentMouse.y <= Mathf.Max(emptyTilePos.y,selectedTile.initPosition.y)
-            && currentMouse.y >= Mathf.Min(emptyTilePos.y,selectedTile.initPosition.y)))
-            {     
-                selectedTile.transform.position = currentMouse;
-            }
-            
+                Vector2 emptyTilePos = emptyTile.transform.position;
+                Vector2 CurrentPosition = (Vector2)selectedTile.transform.position;
+
+                if ((currentTilePosition.x <= Mathf.Max(emptyTilePos.x, selectedTile.initPosition.x)
+                && currentTilePosition.x >= Mathf.Min(emptyTilePos.x, selectedTile.initPosition.x)) &&
+                (currentTilePosition.y <= Mathf.Max(emptyTilePos.y, selectedTile.initPosition.y)
+                && currentTilePosition.y >= Mathf.Min(emptyTilePos.y, selectedTile.initPosition.y)))
+                {
+                    if (((Vector2)Input.mousePosition - CurrentPosition).x < 70 ||
+                        ((Vector2)Input.mousePosition - CurrentPosition).x > 130)
+                    {
+                        selectedTile.transform.position = new Vector3(currentTilePosition.x, selectedTile.transform.position.y, selectedTile.transform.position.z);
+                    }
+                    if (((Vector2)Input.mousePosition - CurrentPosition).y < -35 ||
+                        ((Vector2)Input.mousePosition - CurrentPosition).y > 35)
+                    {
+                        selectedTile.transform.position = new Vector3(selectedTile.transform.position.x, currentTilePosition.y, selectedTile.transform.position.z);
+                    }
+                }
         }
-        else{
-            mouseTileRatio=Vector2.zero;
+        else
+        {
+            initMousePosition = Vector2.zero;
+            mouseTileDistance = Vector2.zero;
         }
     }
 }
