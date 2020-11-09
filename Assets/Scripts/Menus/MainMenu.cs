5aa185c401f2ca6ef51216eecf9207cd7cace62f
Jesús Orlando Cárcamo Posada <berserker1523@hotmail.com>
2020-11-09 10:37:46 -0500

    menu

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Listens for the onClick events for the main menu menu buttons
+/// </summary>
+public class MainMenu : MonoBehaviour
+{   
+    /// <summary>
+    /// Handles the onClick event from the play button
+    /// </summary>
+    public void HandlePlayButtonOnClickEvent()
+    {
+        SceneManager.LoadScene(SceneName.OverWorld1.ToString());
+    }
+
+    /// <summary>
+    /// Handles the onClcik event from the quit button
+    /// </summary>
+    public void HandleQuitButtonOnClickEvent()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Menus/SceneName.cs
@@ -0,0 +1,4 @@
+﻿public enum SceneName
+{
+    OverWorld1
+}
